17008348dc995dc8e0c329c5bc7672ca9be44fe9
Shubhendu-Pathak <93846723+Shubhendu-Pathak@users.noreply.github.com>
2022-03-26 19:22:27 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import Axios from 'axios'
 import './App.css';
 
 function App() {
+
+const[name,setname]=useState('')
+const[age,setage]=useState(0)
+const[country,setcountry]=useState('')
+const[position,setposition]=useState('')
+const[wage,setwage]=useState(0)
+
+const press=()=>{
+  // console.log(name);
+  Axios.post('http://localhost:3004/create',{
+    name:name,
+    age:age,
+    country:country,
+    position:position,
+    wage:wage
+  }).then(()=>{console.log('SUCCESS')})
+ 
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <label htmlFor="">NAME: <input onChange={(e)=>{setname(e.target.value)}} type="text" name="" id="" /></label>
+     <label htmlFor="">AGE: <input onChange={(e)=>{setage(e.target.value)}} type="number" name="" id="" /></label>
+     <label htmlFor="">COUNTRY: <input onChange={(e)=>{setcountry(e.target.value)}} type="text" name="" id="" /></label>
+     <label htmlFor="">POSITION: <input onChange={(e)=>{setposition(e.target.value)}} type="text" name="" id="" /></label>
+     <label htmlFor="">WAGE: <input onChange={(e)=>{setwage(e.target.value)}} type="number" name="" id="" /></label>
+     <button onClick={press}>Add to DataBase</button>
     </div>
   );
 }
